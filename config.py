5a006c664ad5cbe9f17a7f139c5c2e0fc7092068
Robert Bost <bostrt@gmail.com>
2017-05-02 09:46:34 -0400

    testing

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ DEBUG = True
 DEFAULT_DATA_DIR = '/tmp'
 DEFAULT_PORT = 8888
 XHEADERS = True # you may set this to false if not behind another server
-DB = 'elimage.db'
+DB = '/tmp/elimage.db'
 
-PREFIX = r'/elimage' # For WSGI. this is a regex
+PREFIX = r'/' # For WSGI. this is a regex
